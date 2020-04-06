2916a9f0c5ae45e90c2929e5b7c4d27e570f74a7
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2020-04-06 11:12:23 -0400

    started regression lab

--- LinearRegression/src/DistanceVsTime.py
+++ LinearRegression/src/DistanceVsTime.py
@@ -12,7 +12,7 @@ f.xaxis.axis_label = 'Time'
 f.yaxis.axis_label = 'Distance (m)'
 f.scatter(x=x,y=y)
 f.line(x=x,y=[150-14.2*i for i in x],line_dash='dashed',legend_label='Estimated Speed = -14.2m/s',color='red')
-f.y_range=Range1d(0,150)
+f.y_range=Range1d(0,200)
 export_png(f, filename='../img/distance-vs-time.png')
 show(f)
 
